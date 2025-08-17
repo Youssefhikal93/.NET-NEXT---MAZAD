ebfe1e415925cb4afbea825615a0ffbcb6a16d5a
Youssef Hikal <youssefhikal@gmail.com>
2025-08-17 12:18:10 +0200

    added service search and communication between services

---
+++ src/SearchService/Controllers/SearchController.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Http;
+using MongoDB.Entities;
+using SearchService.Models;
+using Microsoft.AspNetCore.Mvc;
+using SearchService.RequestHelpers;
+
+namespace SearchService.Controllers
+{
+    [Route("api/search")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+
+        [HttpGet]
+        public async Task<ActionResult<List<Item>>> SearchItmes([FromQuery]SearchParams searchParams)
+        {
+            var query = DB.PagedSearch<Item,Item>();
+            
+            // searching
+            if (!string.IsNullOrEmpty(searchParams.SearchTerm))
+            {
+                query.Match(Search.Full, searchParams.SearchTerm).SortByTextScore();
+            }
+
+            if (!string.IsNullOrEmpty(searchParams.Seller))
+            {
+                query.Match(x => x.Seller == searchParams.Seller);
+            }
+             if (!string.IsNullOrEmpty(searchParams.Winner))
+            {
+                query.Match(x => x.Winner == searchParams.Winner);
+            }
+
+            //orderByfilters
+            query = searchParams.OrderBy switch
+            {
+                "make" => query.Sort(x => x.Ascending(a => a.Make)),
+                "new" => query.Sort(x => x.Descending(a => a.CreateDate)),
+                _ => query.Sort(x=>x.Ascending(a=>a.AuctionEnd))
+            };
+
+            //FilterBY
+            query = searchParams.FilterBy switch
+            {
+                "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
+                "endingSoon" => query.Match(x => x.AuctionEnd < DateTime.UtcNow.AddHours(6) && x.AuctionEnd > DateTime.UtcNow),
+                _ => query.Match(x=>x.AuctionEnd>DateTime.UtcNow)
+            };
+
+
+            //pagination
+            query.PageNumber(searchParams.PageNumber);
+            query.PageSize(searchParams.PageSize);
+
+
+            var result = await query.ExecuteAsync();
+
+            return Ok(new
+            {
+
+                pageCount = result.PageCount,
+                totalCount = result.TotalCount,
+                results = result.Results
+            });
+        }
+
+    }
+
+    
+}
---
+++ src/SearchService/Program.cs
@@ -0,0 +1,49 @@
+using System.Net;
+using MongoDB.Driver;
+using MongoDB.Entities;
+using Polly;
+using Polly.Extensions.Http;
+using SearchService.Data;
+using SearchService.Models;
+using SearchService.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddHttpClient<AuctionsSvcHttpClient>().AddPolicyHandler(GetPolicy());
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+
+
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+try
+{
+    
+await DbInitializer.InitDb(app);
+}
+catch (Exception e)
+{
+    
+    System.Console.WriteLine(e);;
+}
+
+app.Run();
+
+//using polling
+
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+=> HttpPolicyExtensions
+.HandleTransientHttpError()
+.OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+.WaitAndRetryForeverAsync(_=>TimeSpan.FromSeconds(3));
