56c112698446939621b435270fa551aa01c8b4ff
Youssef Hikal <youssefhikal@gmail.com>
2025-08-31 14:32:51 +0200

    added fetch wrapper

--- frontend/web-app/app/_lib/actions/auctions.ts
+++ frontend/web-app/app/_lib/actions/auctions.ts
@@ -1,13 +1,12 @@
 "use server";
 import { IAuction, pagedResult } from "@/app/types/types";
-import { auth } from "@/auth";
+
+import { fetchWrapper } from "../fetchWrapper";
+
+
 
 export async function getAuctions(query: string): Promise<pagedResult<IAuction>> {
-    const res = await fetch(`http://localhost:6001/search${query}`);
-
-    if (!res.ok) throw new Error('Failed to fetch data');
-
-    return res.json();
+   return fetchWrapper.get(`search${query}`)
 }
 
 export async function updateAuctointest():Promise<{status:number,message:string}>{
@@ -15,17 +14,33 @@ export async function updateAuctointest():Promise<{status:number,message:string}
         milage: Math.floor(Math.random()*1000)+1
     }
 
-    const session = await auth();
-
-    const res = await fetch(`http://localhost:6001/auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c`, {
-        method:"PUT",
-        headers:{
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${session?.accessToken}`
-        },
-        body:JSON.stringify(data)
-    })
-    if(!res.ok) return {status:res.status,message:res.statusText} 
-
-    return {status:res.status,message:res.statusText}
+    return fetchWrapper.put("auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c",data)
 }
+
+// export async function getAuctions(query: string): Promise<pagedResult<IAuction>> {
+//     const res = await fetch(`http://localhost:6001/search${query}`);
+
+//     if (!res.ok) throw new Error('Failed to fetch data');
+
+//     return res.json();
+// }
+
+// export async function updateAuctointest():Promise<{status:number,message:string}>{
+//     const data = {
+//         milage: Math.floor(Math.random()*1000)+1
+//     }
+
+//     const session = await auth();
+
+//     const res = await fetch(`http://localhost:6001/auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c`, {
+//         method:"PUT",
+//         headers:{
+//             "Content-Type": "application/json",
+//             "Authorization": `Bearer ${session?.accessToken}`
+//         },
+//         body:JSON.stringify(data)
+//     })
+//     if(!res.ok) return {status:res.status,message:res.statusText} 
+
+//     return {status:res.status,message:res.statusText}
+// }
---
+++ frontend/web-app/app/_lib/fetchWrapper.ts
@@ -0,0 +1,71 @@
+import { auth } from "@/auth";
+
+const baseUrl = "http:localhost:6001/";
+
+
+async function get(url:string) {
+    const requestOptions = {
+        method: "GET",
+         headers:await getHeaders(),
+    }
+
+    const res = await fetch(baseUrl + url , requestOptions)
+    return handelResponse(res)
+}
+async function put(url:string,body:unknown) {
+    const requestOptions = {
+        method: "PUT",
+        headers:await getHeaders(),
+        body: JSON.stringify(body)
+    }
+
+    const res = await fetch(baseUrl + url , requestOptions)
+    return handelResponse(res)
+}
+async function post(url:string,body:unknown) {
+    const requestOptions = {
+        method: "POST",
+        headers:await getHeaders(),
+        body: JSON.stringify(body)
+    }
+
+    const res = await fetch(baseUrl + url , requestOptions)
+    return handelResponse(res)
+}
+async function del(url:string) {
+    const requestOptions = {
+        method: "DELETE",
+        headers:await getHeaders(),
+        
+    }
+
+    const res = await fetch(baseUrl + url , requestOptions)
+    return handelResponse(res)
+}
+
+async function handelResponse(res: Response) {
+    const text = await res.text();
+    const data = text && JSON.parse(text);
+
+    if(res.ok){
+        return data || res.statusText
+    }else {
+        const error = {
+            status: res.status,
+            message:res.statusText
+        }
+        return error;
+    }
+}
+
+async function getHeaders(): Promise<Headers>{
+    const session = await auth();
+    const headers = new Headers();
+    headers.set("Content-type","application/json")
+    if(session){
+        headers.set("Authorization","Bearer "+session.accessToken)
+    }
+    return headers
+}
+
+export const fetchWrapper = {get , post,put,del}
