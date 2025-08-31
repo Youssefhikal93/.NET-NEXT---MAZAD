37809aa95619ea060db9150673945b53087d9614
Youssef Hikal <youssefhikal@gmail.com>
2025-08-31 17:09:44 +0200

    added modal instead of confirm window

--- frontend/web-app/app/auctions/details/[id]/DeleteButton.tsx
+++ frontend/web-app/app/auctions/details/[id]/DeleteButton.tsx
@@ -1,24 +1,26 @@
 "use client"
 import { deleteAuction } from '@/app/_lib/actions/auctions';
-import { Button, Spinner } from 'flowbite-react'
+import { Button, Modal, ModalBody, ModalHeader, Spinner } from 'flowbite-react'
 import { useRouter } from 'next/navigation';
 import React, { useState } from 'react'
 import toast from 'react-hot-toast';
-import DeleteModal from './DeleteModal';
+import { HiOutlineExclamationCircle } from "react-icons/hi";
+
 type Props = {
     id:string
 }
 export default function DeleteButton({id}:Props) {
     const [loading,setLoading]=useState(false);
-     const [openModal, setOpenModal] = useState(true);
+     const [openModal, setOpenModal] = useState(false);
     const router = useRouter()
 
     function handelDelete(){
         setLoading(true)
         
-       const result = confirm("are you sure that you want to delete?");
-        if (result) {
-    //Logic to delete the item
+        //in case window confirm is required
+    //    const result = confirm("are you sure that you want to delete?");
+    //     if (result) {
+        // }
 
              deleteAuction(id).then(res=>{
                 if(res.error){
@@ -36,12 +38,32 @@ export default function DeleteButton({id}:Props) {
         
         .finally(()=> setLoading(false))
            
-    }
 }
   return (
-     <Button  color="red" onClick={(handelDelete)}>
+    <>
+     <Button  color="red" onClick={() => setOpenModal(true)}>
         {loading && <Spinner size='sm'/>}
         Delete Auction 
     </Button>
+ <Modal show={openModal} size="md" onClose={() => setOpenModal(false)} popup>
+        <ModalHeader />
+        <ModalBody>
+          <div className="text-center">
+            <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
+            <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+              Are you sure you want to delete this product?
+            </h3>
+            <div className="flex justify-center gap-4">
+              <Button color="red" onClick={handelDelete}>
+                Yes, I&apos;m sure
+              </Button>
+              <Button color="alternative" onClick={() => setOpenModal(false)}>
+                No, cancel
+              </Button>
+            </div>
+          </div>
+        </ModalBody>
+      </Modal>
+    </>
   )
 }
